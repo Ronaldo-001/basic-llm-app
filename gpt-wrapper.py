5bb4024b766ecdd5b1d955c1f683d6b48085610d
Ronaldo-001 <ronaldop1705@gmail.com>
2025-01-13 09:57:36 +0000

    created image and text generation in gpt

--- gpt-wrapper.py
+++ gpt-wrapper.py
@@ -2,15 +2,34 @@
 
 #import dependencies
 from openai import OpenAI
-from streamlit as st
+import streamlit as st
+import requests
 
 api_key= st.secrets["api_key"]    #import api from streamlit secrets
 client = OpenAI(api_key=api_key)  #adding api key from openai
-completion = client.chat.completions.create(    #function call to openai (arguments-model-messages)
-    model="gpt-4o-mini",          #model to use 
-    #store=True,
-    messages=[
-        {"role": "user", "content": "write about life"} # specify user and their prompt in content
-    ]
-)
-print(completion.choices[0].message.content) # here the respose from gpt is in json so we use . format to access the answer
+
+def generate_content(prompt):
+    completion = client.chat.completions.create(    #function call to openai (arguments-model-messages)
+        model="gpt-4o-mini",          #model to use 
+        #store=True,
+        messages=[
+            {"role": "user", "content": prompt} # specify user and their prompt in content
+        ]
+    )
+    return completion.choices[0].message.content # here the respose from gpt is in json so we use . format to access the answer
+
+def generate_image(prompt):
+    response = client.images.generate(  #calling image gen function with prompts
+        model="dall-e-3",               #parameters of the function
+        prompt="a boy facing his dreams",
+        size="1024x1024",
+        quality="standard",
+        n=1,  #quantity of images
+    )
+    #print(response.data[0].url) #url link to download generated image
+    image_url= (response.data[0].url) 
+
+    image_response = requests.get(image_url) # use requests lib to downlaod image using api request
+
+    with open("images/new.png", 'wb') as file: #name the downloaded file and save to local path
+        file.write(image_response.content)  # calling file write function to save
